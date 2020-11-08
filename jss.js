5877423fffc1d1208c1da961752dcc9f6468278a
dinkumehta <mehtadinku@gmail.com>
2020-11-08 20:17:52 +0530

    ramdom number soleved

--- jss.js
+++ jss.js
@@ -3,10 +3,6 @@ var photos = [];
 
 function nextbtn() {
 
-
-
-
-
     while (true) {
         if (photos.length === 5) {
             photos = []
@@ -18,15 +14,8 @@ function nextbtn() {
             }
         }
     }
-    console.log(photos)
-    console.log(last_number)
-        //radom_number for the img
-        // last_number = (Math.floor(Math.random() * 5) + 1);
-
-    // var random_done = [];
-    // for(var i=0;i<no_of_memes;i++) {
-    //     random_done[i]=false;
-    // }
+    // console.log(photos)
+    // console.log(last_number)
     document.getElementById("meme").src = 'images/' + (last_number) + '.jpg ';
 
 
