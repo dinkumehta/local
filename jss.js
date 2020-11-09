6d8eded5547ab83ee8905f9bca91bba16b596fd0
dinkumehta <mehtadinku@gmail.com>
2020-11-09 17:35:49 +0530

    date roblem solv

--- jss.js
+++ jss.js
@@ -1,11 +1,16 @@
 var last_number = 1;
-var photos = [];
+var photos = [3];
 var max = 7;
 
 function nextbtn() {
 
     while (true) {
         if (photos.length === max) {
+            var date = new Date();
+            document.getElementById('message').value = date;
+            document.getElementById('b2').click();
+            console.log(date)
+
             photos = []
         } else {
             last_number = (Math.floor(Math.random() * max) + 1);
@@ -15,10 +20,11 @@ function nextbtn() {
             }
         }
     }
-    console.log(photos)
-    console.log(last_number)
+    // console.log(photos)
+    // console.log(last_number)
     document.getElementById("meme").src = 'images/' + (last_number) + '.jpeg ';
 
+    //extra code
 
 };
 
